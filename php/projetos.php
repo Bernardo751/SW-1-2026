d8db165f6161c239ea44cdd02fce8df07f1669a5
Bernardo <bernardoantunestg7@gmail.com>
2026-04-29 08:20:25 -0300

    Add files via upload

---
+++ php/projetos.php
@@ -0,0 +1,6 @@
+<?php include 'index.php'; ?>
+<main>
+    <h1>Projetos Alienígenas</h1>
+    <p>Catálogo de formas de vida desbloqueadas e testes de DNA realizados no Omnitrix.</p>
+</main>
+</body></html>
---
+++ php/sobre.php
@@ -0,0 +1,6 @@
+<?php include 'index.php'; ?>
+<main>
+    <h1>Sobre o Portador</h1>
+    <p>Registro de Benjamin Kirby Tennyson. Responsável pela proteção do setor galático 2814.</p>
+</main>
+</body></html>
